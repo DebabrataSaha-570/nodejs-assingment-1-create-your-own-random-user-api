99846e58cbbc127934400ec54eadd024cc6551ff
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2022-09-10 08:38:53 +0600

    all user data loaded from the json file

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const PORT = 5000
 
 app.get('/', (req, res) => {
     res.send('Assingment 1 - Create your own random user API')
+})
+
+app.get('/user/all', (req, res) => {
+    fs.readFile('users.json', (err, data) => {
+        if(err){
+            res.send('Failed to load data')
+        }
+        else{
+           res.send(data)
+        }
+    })
 })
 
 
