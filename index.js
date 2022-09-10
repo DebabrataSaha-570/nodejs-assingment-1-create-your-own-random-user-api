b81226ee88e81abf92b235492b57de1f09e0d627
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2022-09-10 09:11:13 +0600

    get random user api added

--- index.js
+++ index.js
@@ -7,13 +7,31 @@ app.get('/', (req, res) => {
     res.send('Assingment 1 - Create your own random user API')
 })
 
+//get random user api
+
+app.get('/user/random', (req, res) => {
+    fs.readFile('./users.json',  (err, data) =>{
+        if(err){
+            res.send("Failed to load data")
+        }
+        else{
+            const userData = JSON.parse(data)
+            const values = Object.values(userData)
+            const randomValue = values[parseInt(Math.random() * values.length)]
+            res.send(randomValue)
+        }
+    })
+})
+
+// get all user api
+
 app.get('/user/all', (req, res) => {
     fs.readFile('users.json', (err, data) => {
         if(err){
             res.send('Failed to load data')
         }
         else{
-           res.send(data)
+           res.send(JSON.parse(data))
         }
     })
 })
