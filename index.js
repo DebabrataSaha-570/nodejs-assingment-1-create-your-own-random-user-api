0aae1febe3444a78e9fd5bc70be8bfe3ea1be42d
DebabrataSaha-570 <sahadebabrata570@gmail.com>
2022-09-12 19:39:17 +0600

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 const cors = require('cors')
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 
 app.use(cors())
